fba859498af1dc214e3de07492fcdf74ee12d0d6
tm-minhtang <minh.tang@tomosia.com>
2020-12-13 15:05:50 +0700

    change language

--- assets/js/languages/en.js
+++ assets/js/languages/en.js
@@ -5,5 +5,49 @@ const LANG_EN = {
     "text3": "Portfolio",
     "text4": "Workflow",
     "text5": "Contact",
+  },
+  "service": {
+    "title": "OUR SERVICES",
+    "desc": "Lid est laborum dolo rumes fugats untras. Etharums ser quidem rerum facilis dolores nemis omnis fugats vitaes nemo minima rerums unsers sadips amets.",
+    "web_app": {
+      "title": "Api / Web App",
+      "desc": "Fringilla augue at maximus vestibulum. Nam pulvinar vitae neque et porttitor. Praesent sed nisi eleifend.",
+    },
+    "mobile_app": {
+      "title": "Mobile App",
+      "desc": "Fringilla augue at maximus vestibulum. Nam pulvinar vitae neque et porttitor. Praesent sed nisi eleifend.",
+    },
+    "ecommerce": {
+      "title": "Ecommerce",
+      "desc": "Fringilla augue at maximus vestibulum. Nam pulvinar vitae neque et porttitor. Praesent sed nisi eleifend.",
+    },
+    "scrapping": {
+      "title": "Scrapping",
+      "desc": "Fringilla augue at maximus vestibulum. Nam pulvinar vitae neque et porttitor. Praesent sed nisi eleifend.",
+    },
+    "chatbot": {
+      "title": "Chatbot",
+      "desc": "Fringilla augue at maximus vestibulum. Nam pulvinar vitae neque et porttitor. Praesent sed nisi eleifend.",
+    },
+    "tool": {
+      "title": "Tools",
+      "desc": "Fringilla augue at maximus vestibulum. Nam pulvinar vitae neque et porttitor. Praesent sed nisi eleifend.",
+    },
+  },
+  "workflow": {
+    "title": "WORK FLOW",
+    "desc": "Phasellus fringilla suscipit risus nec eleifend. Pellentesque eu quam sem, ac malesuada leo sem quam pellente. Awesome sliders give you the opportunity to showcase your content.",
+    "step1": "MEGA MENU",
+    "step2": "UNLIMITED PORTFOLIO GRID",
+    "step3": "HUGE SHORTCODE",
+    "step4": "MEGA MENU",
+    "step5": "UNLIMITED PORTFOLIO GRID",
+  },
+  "technology": {
+    "title": "AVAILABLE TECHNOLOGIES",
+  },
+  "contact": {
+    "title": "Contact",
+    "desc": "Nam libero tempore, cum soluta nobis est eligendi optio cumque nihil impedit quo minus id quod maxime placeat facere possimus.Nam libero tempore",
   }
 };
--- assets/js/languages/vi.js
+++ assets/js/languages/vi.js
@@ -5,5 +5,49 @@ const LANG_VI = {
     "text3": "Dự án",
     "text4": "Quy trình làm việc",
     "text5": "Liên hệ",
+  },
+  "service": {
+    "title": "Dịch vụ",
+    "desc": "Terabyte-labo là trung tâm nghiên cứu, hợp tác và phát triển các công nghệ chuyên về Công Nghệ Web Application, microservice, ecommerce, Chatbot và ứng dụng trên điện thoại",
+    "web_app": {
+      "title": "Api / Web App",
+      "desc": "Sử dụng Ruby on Rails, Nodejs, Laravel,.. để xây dựng các Web application. Chúng tôi có thể đảm nhiệm Front-end, Back-end cho đến triển khai trên Server.",
+    },
+    "mobile_app": {
+      "title": "Mobile App",
+      "desc": "Sử dụng Swift, Flutter, React Native,.. để xây dựng các ứng dụng trên điện thoại với đội ngũ nghiên cứu đầy kinh nghiệm.",
+    },
+    "ecommerce": {
+      "title": "Ecommerce",
+      "desc": "Với đội ngũ nghiên cứu nhiều năm kinh nghiệm, chúng tôi có thể đảm bảo sản phẩm mang lại tin cậy - chất lượng - hạnh phúc cho cả chủ sở hữu và người dùng.",
+    },
+    "scrapping": {
+      "title": "Scrapping",
+      "desc": "Fringilla augue at maximus vestibulum. Nam pulvinar vitae neque et porttitor. Praesent sed nisi eleifend.",
+    },
+    "chatbot": {
+      "title": "Chatbot",
+      "desc": "Sử dụng API của LINE, Instagram và Facebook Messenger, chúng tôi đề xuất việc sử dụng Chatbot để tối ưu việc giao tiếp với khách hàng.",
+    },
+    "tool": {
+      "title": "Tools",
+      "desc": "Fringilla augue at maximus vestibulum. Nam pulvinar vitae neque et porttitor. Praesent sed nisi eleifend.",
+    },
+  },
+  "workflow": {
+    "title": "Quy trình làm việc",
+    "desc": "Hợp tác với chúng tôi sẽ tạo cơ hội cho bạn để phát triển và tiếp cận các công nghệ cao, chất lượng tốt và tối ưu nhất.\nChúng tôi ưu tiên việc hợp tác và phát triển trong thời gian trung - dài. Với việc MTG hàng tuần, mang lại sự tối ưu và tốc độ phát triển tốt với nhiệm vụ được đề ra.",
+    "step1": "Liên hệ với chúng tôi",
+    "step2": "Trao đổi trực tiếp",
+    "step3": "Ước tính và ký hợp đồng",
+    "step4": "Phát triển sản phẩm",
+    "step5": "Release sản phẩm",
+  },
+  "technology": {
+    "title": "Công Nghệ Sử Dụng",
+  },
+  "contact": {
+    "title": "Liên Hệ",
+    "desc": "Vui lòng liên hệ với chúng tôi về ý kiến, hoặc tư vấn của bạn.\n Chúng tôi sẽ liên hệ với bạn sau khi xác nhận nội dung.",
   }
 };
