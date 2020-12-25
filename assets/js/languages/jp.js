c29f48f5ae8ce27dc728dcd4dcf7ff8b23f062fe
tm-minhtang <minh.tang@tomosia.com>
2020-12-25 14:48:07 +0700

    done lang jp

--- assets/js/languages/jp.js
+++ assets/js/languages/jp.js
@@ -1,9 +1,58 @@
 const LANG_JP = {
   "nav": {
-    "text1": "JP Trang chủ",
-    "text2": "JP Dịch vụ",
-    "text3": "JP Dự án",
-    "text4": "JP Quy trình làm việc",
-    "text5": "JP Liên hệ",
+    "text1": "ホームページ",
+    "text2": "サービス",
+    "text4": "ワークフロー",
+    "text5": "連絡先",
+  },
+  "service": {
+    "title": "サービス",
+    "desc": "Terabyte-laboは、Webアプリケーション、マイクロサービス、Eコマース、チャットボット、およびモバイルアプリケーションに特化したテクノロジーの研究、協力、および開発センターです。",
+    "web_app": {
+      "title": "Api / Web App",
+      "desc": "Ruby on Rails、Nodejs、Laravel、..を使用してWebアプリケーションを構築します。フロントエンド、バックエンドからサーバーへの展開を処理できます。",
+    },
+    "mobile_app": {
+      "title": "Mobile App",
+      "desc": "Swift、Flutter、React Nativeなどを使用して、経験豊富な研究者のチームでモバイルアプリを構築します。",
+    },
+    "ecommerce": {
+      "title": "Ecommerce",
+      "desc": "長年の経験を持つチームにより、所有者とユーザーの両方に信頼性、品質、幸福をもたらす製品を保証できます。",
+    },
+    "scrapping": {
+      "title": "Scrapping",
+      "desc": "Terabye-labo is also highly experience with Web Scraper/Web Crawler, we are able to apply this technologies to also most all of the currently existing website.",
+    },
+    "chatbot": {
+      "title": "Chatbot",
+      "desc": "LINE、Instagram、Facebook Messenger APIを使用して、チャットボットを使用して顧客との通信を最適化することをお勧めします。",
+    },
+    "tool": {
+      "title": "Tools",
+      "desc": "Terabyte-labo also specializes in creating, designing and developing new functions for your existing systems. We prioritize speedy development with exceptional quality at a reasonable cost.",
+    },
+  },
+  "workflow": {
+    "title": "ワークフロー",
+    "desc": "私たちと協力することで、ハイテク、高品質、低コスト、最適なものを開発してアクセスする機会が生まれます。\n私たちは常に中長期的な協力と開発を優先します。毎週のMTGにより、割り当てられたタスクで最適で優れた開発速度を実現します。",
+    "step1": "直接交換 - 顧客から要件の取得",
+    "step2": "時間の見積もり、チームの展開と計画",
+    "step3": "Agile Scrum モデルにより、Dev-BrSE-PM-Comtor-Testerでチームの展開",
+    "step4": "製品開発",
+    "step5": "製品のリリース",
+  },
+  "technology": {
+    "title": "使用するテクノロジー"
+  },
+  "contact": {
+    "title": "連絡先",
+    "desc": "コメント、アドバイス、お問い合わせはお問い合わせください。\n内容確認後、お早めにご連絡いたします。",
+    "text_location": "本社",
+    "headquarter": "20 / H05 / K408\nトンダン、ダナン",
+    "text_call_us": "電話番号",
+    "phone_number": "Any time. We are open 24/7 \n (+84) 917 597 990",
+    "text_email": "メール",
+    "email_address": "lan.le@terabyte-labo.com",
   }
 };
